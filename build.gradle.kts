24d18fbb2f9b0350f0000e24c67ba19cccb4009e
cherepokivan <107845995+cherepokivan@users.noreply.github.com>
2026-07-05 11:12:59 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "9.4.3"
+}
+
+group = "me.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    implementation("net.dv8tion:JDA:6.4.2")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+}
